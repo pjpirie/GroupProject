5dd694b1d41da043c87de6ee083dfcafc4bcd982
--get <pauljohnpirie@yahoo.co.uk>
2021-03-11 22:15:05 +0000

    (+ added updated navbar to master)

--- recruitment-app/src/components/Header/Navbar.js
+++ recruitment-app/src/components/Header/Navbar.js
@@ -1,25 +1,102 @@
 import React from 'react';
 import './Navbar.css';
 import logo from '../logo.svg';
-import {Navbar, Nav} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
   return (
-  <Navbar collapseOnSelect expand="lg" variant="light" className="navbar">
-    <Navbar.Brand href="#home"><img src={logo} className="d-inline-block align-top logo" alt="Website Logo" /></Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#" className="nav-link">Home</Nav.Link>
-        <Nav.Link href="#" className="nav-link">Our Company</Nav.Link>
-        <Nav.Link href="#" className="nav-link">Modules</Nav.Link>
-        <Nav.Link href="#" className="nav-link">Contact</Nav.Link>
-        <Nav.Link href="#"><AccountCircleIcon /></Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+    <Fragment>
+      <Navbar collapseOnSelect expand="lg" variant="light" className="navbar">
+        <Navbar.Brand href="#home"><img src={logo} className="d-inline-block align-top logo" alt="Website Logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Link style={navStyle} to="/">
+              <li className="nav-link">Home</li>
+            </Link>
+            <Link style={navStyle} to="/about">
+              <li className="nav-link">Our Company</li>
+            </Link>
+            <Link style={navStyle} to="/modules">
+              <li className="nav-link">Modules</li>
+            </Link>
+            <Link style={navStyle} to="/contact">
+              <li className="nav-link">Contact</li>
+            </Link>
+            <Link style={navStyle} to="/account">
+              <li onClick={() => loginsetModalIsOpen(true)}><AccountCircleIcon /></li>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal
+        isOpen={loginmodalIsOpen}
+        onRequestClose={() => loginsetModalIsOpen(false)}
+        style={{
+          overlay: {
+            // backgroundColor: 'rgba(33,49,66,0.5)',
+            backgroundImage: 'url(' + loginBG + ')',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+          },
+          content: {
+            color: '#213142',
+            width: '35vw',
+            minWidth: '500px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            bottom: '0',
+            right: '0',
+            translate: '-50% -50%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '20px',
+            padding: '10px'
+          }
+        }}
+      >
+        <Login openRegister={() => {
+          loginsetModalIsOpen(false)
+          registersetModalIsOpen(true)
+        }} />
+      </Modal>
+      <Modal
+        isOpen={registermodalIsOpen}
+        onRequestClose={() => registersetModalIsOpen(false)}
+        style={{
+          overlay: {
+            // backgroundColor: 'rgba(33,49,66,0.5)',
+            backgroundImage: 'url(' + loginBG + ')',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+          },
+          content: {
+            color: '#213142',
+            width: '35vw',
+            minWidth: '500px',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            bottom: '0',
+            right: '0',
+            translate: '-50% -50%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '20px',
+            padding: '10px'
+          }
+        }}
+      >
+        <Register />
+      </Modal>
+    </Fragment>
   )
 }
 
