fc989fd4813f6cecf68de189dd13289b1ac913a4
Christopher Farrell <cfman2.9.00@gmail.com>
2021-03-17 11:29:13 +0000

    Housekeeping 2: Eletric boogaloo

--- recruitment-app/src/App.js
+++ recruitment-app/src/App.js
@@ -1,20 +1,33 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 import Navbar from './components/Header/Navbar.js';
 import Footer from './components/Footer/Footer.js';
+import Landing from './pages/landing';
+import Contact from './pages/contact';
+import Company from './pages/company';
+import Modules from './pages/modules';
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Register />
-      <Login />
+    <Router>
+      <div className="App">
+        <div className="main">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/company" component={Company} />
+            <Route path="/modules" component={Modules} />
+          </Switch>
+        </div>
+      </div>
       <Footer />
-
-    </div>
+    </Router>
   );
 }
 
--- recruitment-app/src/components/Header/Navbar.js
+++ recruitment-app/src/components/Header/Navbar.js
@@ -4,6 +4,7 @@ import logo from '../logo.svg';
 import {Navbar, Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
@@ -12,10 +13,10 @@ function Header() {
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#" className="nav-link">Home</Nav.Link>
-        <Nav.Link href="#" className="nav-link">Our Company</Nav.Link>
-        <Nav.Link href="#" className="nav-link">Modules</Nav.Link>
-        <Nav.Link href="#" className="nav-link">Contact</Nav.Link>
+        <Nav.Link href="#" className="nav-link" to="/">Home</Nav.Link>
+        <Nav.Link href="#" className="nav-link" to="/company">Our Company</Nav.Link>
+        <Nav.Link href="#" className="nav-link" to="/modules">Modules</Nav.Link>
+        <Nav.Link href="#" className="nav-link" to="/contact">Contact</Nav.Link>
         <Nav.Link href="#"><AccountCircleIcon /></Nav.Link>
       </Nav>
     </Navbar.Collapse>
