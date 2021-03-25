effc2796fec3650489db2c8635e11b85564ec76a
Christopher Farrell <cfman2.9.00@gmail.com>
2021-03-25 18:13:12 +0000

    Navbar changes

--- recruitment-app/src/components/Header/Navbar.js
+++ recruitment-app/src/components/Header/Navbar.js
@@ -1,141 +1,62 @@
-import React, { useState, Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navbar.css';
-import { Navbar, Nav } from 'react-bootstrap';
-import Modal from 'react-modal';
 import AccountSection from '../AccountSection/AccountSection';
+import './Navbar.css'
 import { useSelector,useDispatch } from 'react-redux';
 import {setLightNav} from '../../actions';
 
-Modal.setAppElement('#root');
 
 
+function Navbar(props) {
+
+    
 const navStyle = {
-  color: 'rgba(0,0,0,1)',
-  textDecoration: 'none',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center'
+    color: 'rgba(0,0,0,1)',
+    textDecoration: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
 };
 const getLinkStyle = (theme) => {
-  if(theme === true){
-    return {color: 'rgba(255,255,255,1)'}
-  }else{
-    return {color: 'rgba(0,0,0,1)'}
-  }  
+    if(theme === true){
+        return {color: 'rgba(255,255,255,1)'}
+    }else{
+        return {color: 'rgba(0,0,0,1)'}
+    } 
 }
 
-
-
-
-
-function Header(props) {
-  
-
-  
 const dispatch = useDispatch();
 let isNavLight = useSelector(state => state.isLightNav);
   // const [loginmodalIsOpen, loginsetModalIsOpen] = useState(false);
   // const [registermodalIsOpen, registersetModalIsOpen] = useState(false);
-  const setLightStyle = (data) => {
-    dispatch(setLightNav(data))
-  }
+    const setLightStyle = (data) => {
+        dispatch(setLightNav(data))
+    }
 
-  useEffect(() => {
-    props.checkAuth();
-  },[]);
+    useEffect(() => {
+        props.checkAuth();
+    },[]);
 
-  return (
-    <Fragment>
-      <Navbar collapseOnSelect expand="lg" variant="light" className="navbar">        
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Link onClick={() => setLightStyle(true)} style={navStyle} to="/">
-              <li style={getLinkStyle(isNavLight)} className="nav-link">Home</li>
-            </Link>
-            <Link onClick={() => setLightStyle(false)} style={navStyle} to="/modules">
-              <li style={getLinkStyle(isNavLight)} className="nav-link">Modules</li>
-            </Link>
-            <Link onClick={() => setLightStyle(false)} style={navStyle} to="/contact">
-              <li style={getLinkStyle(isNavLight)} className="nav-link">Contact</li>
-            </Link>
-            <AccountSection getLinkStyle={getLinkStyle} setLightStyle={setLightStyle} navStyle={navStyle} lightStyle={isNavLight}/>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    return (
 
 
+        <div className="navbar">
+            <nav className="nav">
+                <Link onClick={() => setLightStyle(false)} style={navStyle} to='/'>
+                    <li style={getLinkStyle(isNavLight)} className="nav-link">Home</li>
+                </Link>
+                <Link onClick={() => setLightStyle(false)} style={navStyle} to='/modules'>
+                    <li style={getLinkStyle(isNavLight)} className="nav-link">Information</li>
+                </Link>
+                <Link onClick={() => setLightStyle(false)} style={navStyle} to='/contact'>
+                    <li style={getLinkStyle(isNavLight)} className="nav-link">Contact</li>
+                </Link>
 
+                <AccountSection getLinkStyle={getLinkStyle} setLightStyle={setLightStyle} navStyle={navStyle} lightStyle={isNavLight}/>
+            </nav>
 
-      {/* <Modal
-        isOpen={loginmodalIsOpen}
-        onRequestClose={() => loginsetModalIsOpen(false)}
-        style={{
-          overlay: {
-            // backgroundColor: 'rgba(33,49,66,0.5)',
-            backgroundImage: 'url(' + loginBG + ')',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'
-          },
-          content: {
-            color: '#213142',
-            width: '35vw',
-            minWidth: '500px',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            bottom: '0',
-            right: '0',
-            translate: '-50% -50%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: '20px',
-            padding: '10px'
-          }
-        }}
-      >
-        <Login openRegister={() => {
-          loginsetModalIsOpen(false)
-          registersetModalIsOpen(true)
-        }} />
-      </Modal>
-      <Modal
-        isOpen={registermodalIsOpen}
-        onRequestClose={() => registersetModalIsOpen(false)}
-        style={{
-          overlay: {
-            // backgroundColor: 'rgba(33,49,66,0.5)',
-            backgroundImage: 'url(' + loginBG + ')',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'
-          },
-          content: {
-            color: '#213142',
-            width: '35vw',
-            minWidth: '500px',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            bottom: '0',
-            right: '0',
-            translate: '-50% -50%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: '20px',
-            padding: '10px'
-          }
-        }}
-      >
-        <Register />
-      </Modal> */}
-    </Fragment>
-  )
+        </div>
+    )
 }
 
-export default Header
+export default Navbar
--- recruitment-app/src/pages/Contact/contact.js
+++ recruitment-app/src/pages/Contact/contact.js
@@ -3,26 +3,15 @@ import {Button} from '@material-ui/core';
 import emailjs from 'emailjs-com'
 import './contact.css'
 
-import {Line} from 'react-lineto';
-
-
 function Contact() {
 
-    function values (){
-        const v = ["10", "20", "30", "40"]
-    } 
-
     const [display, setDisplay] = useState(false)
-    // const [number, setNumber] = useState()
 
     const handelSubmit = (e) =>{
 
         e.preventDefault()
         
         setDisplay(true)
-
-        console.log('s')
-
         
         emailjs.sendForm('service_iqjz62q', 'template_i5kvdhs', e.target, 'user_hjOhdBEySuWYMcZA2nuWV')
             .then((result) => {
@@ -32,13 +21,6 @@ function Contact() {
             });
         // e.target.reset()
     }
-
-    // const checkNumber = num =>{
-    //     if(num.value.match(/^\d{10}$/)){
-    //      onChange={e => checkNumber(e.target.value)} 
-    //     }
-    //     console.log(num.value)
-    // }
 
     return (
         <div className="contact-page">
@@ -54,12 +36,12 @@ function Contact() {
 
                     <label className="label">
                         <p>Email</p>
-                        <input type="email" className="input" required name="email" />
+                        <input type="email" className="input" required name="email"/>
                     </label>
                     
                     <label className="label">
                         <p>Number</p>
-                        <input type="text" className="input" required name="number" />
+                        <input type="text" className="input" required name="number" pattern="^\d{11}$" />
                     </label>
 
                     <label className="label">
@@ -74,7 +56,7 @@ function Contact() {
 
                     <label className="label">
                         <p>Enquiry</p>
-                        <textarea className="textarea" rows="5" cols="74" required name="enquiry" />
+                        <textarea className="textarea" rows="5" cols="74" required name="enquiry"/>
                     </label>
 
                     <Button type="submit" className="submitBtn">Submit</Button>
@@ -86,7 +68,6 @@ function Contact() {
                 <h1 className="formSubmit">Thank you for contacting us with your enquiry</h1>
                 : null
                 }
-                <Line x0={values.v(0)} y0={values.v(1)} x1={values.v(2)} y1={values.v(3)} borderWidth={10} />
                 </div>
         </div>
     );
