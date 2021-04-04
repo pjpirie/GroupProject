62e57fd5cb29f89fe72d24b85c8fd5f0959e7d01
--get <pauljohnpirie@yahoo.co.uk>
2021-04-04 13:33:01 +0100

    (Merge Debugged 2)

--- recruitment-app/src/pages/Modules/3/Module.js
+++ recruitment-app/src/pages/Modules/3/Module.js
@@ -6,6 +6,10 @@ import './Module.css';
 
 
 function Module() {
+
+    const handleDownload = () => {
+        alert("//TODO Download");
+    };
     return (
         <div className="Module__Page__Wrapper">
             <div className="Module__Page__Header">
@@ -14,7 +18,7 @@ function Module() {
             </div>
             <div className="Module__Page__Body">
                 <div className="Module__Page__Video__Container">
-                    <video id="Video">
+                    <video id="Video" autoplay>
                         <source src={Video}></source>
                     </video>
                 </div>
@@ -22,10 +26,14 @@ function Module() {
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet</p>
                 </div>
                 <div className="Module__Page__LearningPoints">
+                    <h2>Key Learning Points</h2>
                     <div className="Module__Page__Point">
                         <div className="Module__Page__Point__Header">
                             <h4>01</h4>
                             <h3>Learnging Point 1</h3>
+                        </div>
+                        <div className="Module__Page__Point__Body">
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
                         </div>
                     </div>
                     <div className="Module__Page__Point">
@@ -33,13 +41,30 @@ function Module() {
                             <h4>02</h4>
                             <h3>Learning Point 2</h3>
                         </div>
+                        <div className="Module__Page__Point__Body">
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                        </div>
                     </div>
                     <div className="Module__Page__Points">
                         <div className="Module__Page__Point__Header">
                             <h4>03</h4>
                             <h3>Learning Point 3</h3>
                         </div>
+                        <div className="Module__Page__Point__Body">
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                        </div>
                     </div>
+                </div>
+                <div className="Module__Page__Activity__Container">
+                    <div className="Module__Page__Activity__Header">
+                        <h2>Activity</h2>
+                    </div>
+                    <div className="Module__Page__Activity__Body">
+                        <p>Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+                <div className="Module__Page__Btn__Container">
+                    <button onClick={() => { handleDownload(); }}>Download</button>
                 </div>
             </div>      
         </div>
